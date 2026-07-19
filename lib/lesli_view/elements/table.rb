6c7c61a9d47159e16779834ff48ca7f5274b315e
Luis Donis <ldonis.emc2@gmail.com>
2026-07-19 14:52:41 -0600

    migrate table component to tailwind

--- lib/lesli_view/elements/table.rb
+++ lib/lesli_view/elements/table.rb
@@ -33,50 +33,100 @@ Building a better future, one line of code at a time.
 module LesliView
     module Elements
         class Table < ViewComponent::Base
-            attr_reader :id, :class_name, :pagination, :loading, :headless, :columns, :records, :link
+            attr_reader :id, :class_name, :headless, :columns, :records, :link, :caption, :empty_text
 
             renders_many :rows, "TableRow"
 
-            def initialize(columns: nil, records: nil, id: nil, class_name: "is-striped", pagination: nil, loading: false, headless: false, link: nil)
+            def initialize(
+                columns: nil,
+                records: nil,
+                id: nil,
+                class_name: nil,
+                headless: false,
+                link: nil,
+                caption: nil,
+                empty: nil,
+                striped: true,
+                compact: false
+            )
                 @id = id
                 @class_name = class_name
-                @pagination = pagination
-                @loading = loading
                 @headless = headless
-                @columns = columns
+                @columns = Array(columns).map { |column| column.to_h.symbolize_keys }
                 @records = records
                 @link = link
+                @caption = caption
+                @empty_text = empty || I18n.t("lesli_view.table.empty", default: "No records found")
+                @striped = striped
+                @compact = compact
             end
 
-            def table_header_class(column)
-                column[:field] == "id" || column[:align] == "center" ? "has-text-centered" : nil
+            def table_classes
+                class_names(
+                    "lesli-table",
+                    ("lesli-table--striped" if @striped),
+                    ("lesli-table--compact" if @compact),
+                    class_name
+                )
             end
 
-            def table_body_class(column)
-                column[:field] == "id" || column[:align] == "center" ? "has-text-centered" : nil
+            def column_class(column)
+                alignment = column[:align]&.to_sym
+                alignment = :center if column[:field].to_s == "id"
+                return unless %i[left center right].include?(alignment)
+
+                "lesli-table__cell--#{alignment}"
+            end
+
+            def value_for(record, column)
+                field = column.fetch(:field)
+                field_name = field.to_s
+
+                if record.respond_to?(:attributes) && record.attributes.key?(field_name)
+                    record.attributes[field_name]
+                elsif record.respond_to?(:key?)
+                    return record[field] if record.key?(field)
+                    return record[field_name] if record.key?(field_name)
+                    return record[field_name.to_sym] if record.key?(field_name.to_sym)
+                elsif record.respond_to?(field_name)
+                    record.public_send(field_name)
+                end
+            end
+
+            def empty?
+                records.present? ? records.empty? : rows.empty?
             end
 
             class TableRow < ViewComponent::Base
                 renders_many :cells, "TableData"
 
-                def initialize(css_class: "")
-                    @css_class = css_class
+                STATUSES = %w[danger info success warning].freeze
+
+                def initialize(class_name: nil, status: nil)
+                    @class_name = class_name
+                    @status = status.to_s if STATUSES.include?(status.to_s)
                 end
 
                 def call
-                    # safe_joins ensure multiple <td> elements render correctly
-                    content_tag(:tr, safe_join(cells), class:@css_class)
+                    content_tag(
+                        :tr,
+                        safe_join(cells),
+                        class: class_names(@class_name, @status && "lesli-table__row--#{@status}")
+                    )
                 end
 
                 class TableData < ViewComponent::Base
-                    attr_reader :css_class
+                    attr_reader :class_name
 
-                    def initialize(css_class: "")
-                        @css_class = css_class
+                    def initialize(class_name: nil, align: nil)
+                        @class_name = class_names(
+                            class_name,
+                            ("lesli-table__cell--#{align}" if %i[left center right].include?(align&.to_sym))
+                        )
                     end
                     
                     def call
-                        content_tag :td, content, class: css_class
+                        content_tag(:td, content, class: class_name)
                     end
                 end
             end

f8dbfbe7a1aa56058a127014360f44be41373b21
Luis Donis <ldonis.emc2@gmail.com>
2026-07-19 15:24:24 -0600

    migrate toolbar to tailwind

--- lib/lesli_view/components/toolbar.rb
+++ lib/lesli_view/components/toolbar.rb
@@ -35,12 +35,49 @@ Building a better future, one line of code at a time.
 module LesliView
     module Components
         class Toolbar < ViewComponent::Base
-            attr_reader :search_placeholder, :initial_value, :pagination
-        
-            def initialize(search_placeholder = "Search...", initial_value: nil, pagination: true)
+            renders_one :filters
+            renders_one :actions
+
+            attr_reader :search_placeholder, :initial_value, :url, :search_name
+
+            def initialize(search_placeholder = nil, initial_value: nil, url: nil, search_name: :search)
                 @search_placeholder = search_placeholder
                 @initial_value = initial_value
-                @pagination = pagination
+                @url = url
+                @search_name = search_name
+            end
+
+            def form_url
+                url.presence || request.path
+            end
+
+            def search_value
+                return initial_value unless initial_value.nil?
+
+                request.query_parameters[search_name.to_s]
+            end
+
+            def searching?
+                search_value.present?
+            end
+
+            def clear_url
+                query = request.query_parameters.except(search_name.to_s)
+                return form_url if query.empty?
+
+                "#{form_url}?#{query.to_query}"
+            end
+
+            def placeholder
+                search_placeholder.presence || I18n.t("lesli_view.toolbar.placeholder", default: "Search...")
+            end
+
+            def search_label
+                I18n.t("lesli_view.toolbar.search", default: "Search")
+            end
+
+            def clear_label
+                I18n.t("lesli_view.toolbar.clear", default: "Clear search")
             end
         end
     end
